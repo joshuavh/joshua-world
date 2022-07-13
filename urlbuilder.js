5d7bc871ac0c353c957841d9bc7780282c1a41e1
joshuavh <joshua.vonhofen@googlemail.com>
2022-07-13 14:57:13 +0200

    improved loading screen

---
+++ urlbuilder.js
@@ -0,0 +1,20 @@
+const sanityClient = require('@sanity/client')
+const client = sanityClient({
+  projectId: 'jidqpryp',
+  dataset: 'production',
+  apiVersion: '2021-10-21', // use current UTC date - see "specifying API version"!
+  token: 'sanity-auth-token', // or leave blank for unauthenticated usage
+  useCdn: true, // `false` if you want to ensure fresh data
+})
+
+import imageUrlBuilder from '@sanity/image-url'
+
+// Get a pre-configured url-builder from your sanity client
+const builder = imageUrlBuilder(client)
+
+// Then we like to make a simple function like this that gives the
+// builder an image and returns the builder for you to specify additional
+// parameters:
+function urlFor(source) {
+  return builder.image(source)
+}
