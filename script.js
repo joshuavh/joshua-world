7183340d88a3f967b31af4e5b30d997dc1e058f9
joshuavh <joshua.vonhofen@gmail.com>
2022-08-18 19:39:54 +0200

    Logo + Color improvement

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ fetch(URL)
 
 const stats = new Stats()
 stats.showPanel(0) // 0: fps, 1: ms, 2: mb, 3+: custom
-document.body.appendChild(stats.dom)
+//document.body.appendChild(stats.dom)
 
 const canvas = document.querySelector('canvas.webgl')
 
