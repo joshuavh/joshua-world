15decaa4780402866e5e21e22a1525586791ff0f
joshuavh <joshua.vonhofen@gmail.com>
2022-08-24 10:08:36 +0200

    Added subheadlines

--- sanity.js
+++ sanity.js
@@ -10,6 +10,9 @@ let URL = `https://${PROJECT_ID}.api.sanity.io/v2022-07-11/data/query/${DATASET}
 fetch(URL)
   .then((res) => res.json())
   .then(({ result }) => {
+
+    console.log(result);
+
     let modal = document.getElementsByClassName("modal-content-wrapper"); 
     let thumbnail = document.getElementsByClassName("thumbnail");
     let header = document.getElementsByClassName("header");
@@ -20,12 +23,15 @@ fetch(URL)
 
     for (let i = 0; i < result.length; i++) {
         thumbnail[i].src = "https://cdn.sanity.io/images/jidqpryp/production/" + result[i].thumbnail.asset._ref.substring(6, result[i].thumbnail.asset._ref.length-4) + ".jpg";
-        header[i].style.backgroundImage = 'linear-gradient(to bottom, rgba(0,0,0, 0.25), rgba(28,0,36, 0.75)), url(https://cdn.sanity.io/images/jidqpryp/production/' + result[i].herobanner.asset._ref.substring(6, result[i].herobanner.asset._ref.length-4) + '.jpg)';
+        header[i].style.backgroundImage = 'linear-gradient(to bottom, rgba(0,0,0, 0.2), rgba(0,0,0, 0.70)), url(https://cdn.sanity.io/images/jidqpryp/production/' + result[i].herobanner.asset._ref.substring(6, result[i].herobanner.asset._ref.length-4) + '.jpg)';
         projecttitles[i].textContent = result[i].title;
         subtitles[i].textContent = result[i].subtitle;
         headline[i].textContent = result[i].title;
         modal_subtitles[i].textContent = result[i].subtitle;
         for (let b = 0; b < result[i].sections.length; b++ ){
+            if (result[i].sections[b]._type == 'subheadline'){
+                modal[i].innerHTML += '<h1>' + result[i].sections[b].input + '</h1>';
+            }
             if (result[i].sections[b]._type == 'text-field'){
                 modal[i].innerHTML += '<p2>' + result[i].sections[b].input + '</p2>';
             }
@@ -35,8 +41,6 @@ fetch(URL)
                 newImg.src = "https://cdn.sanity.io/images/jidqpryp/production/" + result[i].sections[b].asset._ref.substring(6, result[i].sections[b].asset._ref.length-4) + ".jpg";
                 modal[i].appendChild(newImg);
             }
-            else {
-            }  
         }
         let credits = document.createElement('div');
         credits.classList.add('credits');
