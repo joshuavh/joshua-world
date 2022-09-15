71c5a221288e63894940bd00c3a4b16466db6635
joshuavh <joshua.vonhofen@gmail.com>
2022-09-15 18:25:47 +0200

    new plant

--- js/3D.js
+++ js/3D.js
@@ -412,7 +412,7 @@ document.getElementById("start-button").onclick = function() {
 
 // Lights
 const hemiLight = new THREE.HemisphereLight( 0xfff, 0xfff, 0.6 );
-hemiLight.color.setHSL( 0.6, 1 , 0.6 );
+hemiLight.color.setHSL( 0.6, 1, 0.6 );
 hemiLight.groundColor.setHSL( 0.095, 1, 0.75 );
 hemiLight.position.set( 0, 500, 0 );
 scene.add( hemiLight );
